d627761d79e94354226cd0e052ac01c3690fd99d
Razmik Dabaghyan <rdabaghyan@yahoo.com>
2022-08-29 03:02:05 -0700

    refactoring W4 learnTogether

--- w4/app.js
+++ w4/app.js
@@ -9,48 +9,19 @@ showOnPage(
   "<b>&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Raz's Daily Decisions for Day</b>"
 );
 
-//Creating an object called dailyDecision that contains the different types of things I am doing during a day.
+//Creating an object called dailyChoice.dailyDecision that contains the different types of things I am doing during a day.
 //Using a method to determine the time and select what thing I has to do based on the time.
-
-const dailyDecision = {
-  runningTime: "lets go for a run",
-  answerEmail: "time to check my emails",
-  lunchBreakTime: "take lunch break",
-  availableTime: "have 1 hour free time",
-  doHomework: "time to do some homework",
-  zoomMeeting: "zoom meeting in 15:30",
-  beforeSleep: "spend some time with my family, watch a movie, play ps5, etc.",
-};
-
-let startTimeDecision = function (hour) {
-  if (hour >= 8 && hour < 9) {
-    return `It's 8 a.m., ${dailyDecision.runningTime}.`;
-  } else if (hour >= 9 && hour <= 10) {
-    return `It's 9 a.m. Okay, ${dailyDecision.answerEmail}. After that, I'll have my lunch break.`;
-  } else if (hour > 10 && hour <= 11) {
-    return `I often skip breakfast and ${dailyDecision.lunchBreakTime} around 11 a.m.`;
-  } else if (hour > 11 && hour <= 13) {
-    return `It's 12 p.m., I ${dailyDecision.availableTime}.`;
-  } else if (hour > 13 && hour < 15) {
-    return ` ----> It's ${dailyDecision.doHomework} <---- `;
-  } else if (hour >= 15 && hour < 17) {
-    return `It's 15 p.m., get ready for ${dailyDecision.zoomMeeting}`;
-  } else if (hour >= 17 && hour < 20) {
-    return `After doing all my homework and zoom meetings, now I can ${dailyDecision.beforeSleep} before going to sleep.`;
-  } else {
-    return `It is night time, go to bed.`;
-  }
-};
-
-showOnPage(startTimeDecision(14));
-
-showOnPage(
-  "<b>--------------------Homework Assignments Weekly Routine---------------------<b>"
-);
-
-// Have a Monday to Friday for doing Homework Assignments.
-// Code showing how my homework assignments routine goes Monday through Friday.
 const dailyChoice = {
+  dailyDecision: {
+    runningTime: "lets go for a run",
+    answerEmail: "time to check my emails",
+    lunchBreakTime: "take lunch break",
+    availableTime: "have 1 hour free time",
+    doHomework: "time to do some homework",
+    zoomMeeting: "zoom meeting in 15:30",
+    beforeSleep:
+      "spend some time with my family, watch a movie, play ps5, etc.",
+  },
   assignmentQuantity: 0,
   partOfAssignments: 0,
 
@@ -76,26 +47,54 @@ const dailyChoice = {
       );
     }
   },
+  startTimeDecision: function (hour) {
+    if (hour >= 8 && hour < 9) {
+      return `It's 8 a.m., ${dailyChoice.dailyChoice.dailyDecision.runningTime}.`;
+    } else if (hour >= 9 && hour <= 10) {
+      return `It's 9 a.m. Okay, ${dailyChoice.dailyDecision.answerEmail}. After that, I'll have my lunch break.`;
+    } else if (hour > 10 && hour <= 11) {
+      return `I often skip breakfast and ${dailyChoice.dailyDecision.lunchBreakTime} around 11 a.m.`;
+    } else if (hour > 11 && hour <= 13) {
+      return `It's 12 p.m., I ${dailyChoice.dailyDecision.availableTime}.`;
+    } else if (hour > 13 && hour < 15) {
+      return ` ----> It's ${dailyChoice.dailyDecision.doHomework} <---- `;
+    } else if (hour >= 15 && hour < 17) {
+      return `It's 15 p.m., get ready for ${dailyChoice.dailyDecision.zoomMeeting}`;
+    } else if (hour >= 17 && hour < 20) {
+      return `After doing all my homework and zoom meetings, now I can ${dailyChoice.dailyDecision.beforeSleep} before going to sleep.`;
+    } else {
+      return `It is night time, go to bed.`;
+    }
+  },
 };
 
-let min = 7
-let max = 10
-let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+showOnPage(dailyChoice.startTimeDecision(14));
 
-let min1 = 7
-let max1 = 10
-let randomNum1 = Math.floor(Math.random() * (max1 - min1 + 1)) + min1
+showOnPage(
+  "<b>--------------------Homework Assignments Weekly Routine---------------------<b>"
+);
 
-let min2 = 7
-let max2 = 10
-let randomNum2 = Math.floor(Math.random() * (max2 - min2 + 1)) + min2
+// Have a Monday to Friday for doing Homework Assignments.
+// Code showing how my homework assignments routine goes Monday through Friday.
+
+const min1 = 7;
+const max1 = 10;
+const randomNum1 = Math.floor(Math.random() * (max1 - min1 + 1)) + min1;
+
+const min2 = 7;
+const max2 = 10;
+const randomNum2 = Math.floor(Math.random() * (max2 - min2 + 1)) + min2;
+
+const min3 = 7;
+const max3 = 10;
+const randomNum3 = Math.floor(Math.random() * (max3 - min3 + 1)) + min3;
 
 showOnPage(`<br><b>Homework Assignments Monday through Friday</b>`);
-dailyChoice.homeworkCheck(35, "Monday Homework Assignments", randomNum, 7);
-dailyChoice.homeworkCheck(28, "Tuesday Homework Assignments", randomNum1, 7);
-dailyChoice.homeworkCheck(20, "Wednesday Homework Assignments", randomNum2, 7);
-dailyChoice.homeworkCheck(13, "Thursday Homework Assignments", randomNum, 7);
-dailyChoice.homeworkCheck(6, "Friday Homework Assignments", randomNum1, 6);
+dailyChoice.homeworkCheck(35, "Monday Homework Assignments", randomNum1, 7);
+dailyChoice.homeworkCheck(28, "Tuesday Homework Assignments", randomNum2, 7);
+dailyChoice.homeworkCheck(20, "Wednesday Homework Assignments", randomNum3, 7);
+dailyChoice.homeworkCheck(13, "Thursday Homework Assignments", randomNum1, 7);
+dailyChoice.homeworkCheck(6, "Friday Homework Assignments", randomNum2, 6);
 showOnPage(
   `<b>${
     dailyChoice.assignmentQuantity - dailyChoice.partOfAssignments
