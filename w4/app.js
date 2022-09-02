af9e9870f97538cad35c4a8971de393c2718aa3d
Razmik Dabaghyan <rdabaghyan@yahoo.com>
2022-09-02 16:29:26 -0700

    w4 learnTogether - writing 1 function instead of 3 (min, max)

--- w4/app.js
+++ w4/app.js
@@ -22,6 +22,7 @@ const dailyChoice = {
     beforeSleep:
       "spend some time with my family, watch a movie, play ps5, etc.",
   },
+
   assignmentQuantity: 0,
   partOfAssignments: 0,
 
@@ -43,10 +44,11 @@ const dailyChoice = {
       );
     } else if (dueAssignment <= dailyScheduledAssignment) {
       showOnPage(
-        `<b>There are no unexpected assignments, which will prevent me from continuing with my previously planned weekly routine.</b>`
+        `<b>There are no unexpected assignments, which will prevent me from continuing with my previously planned weekly routine.</b>`
       );
     }
   },
+
   startTimeDecision: function (hour) {
     if (hour >= 8 && hour < 9) {
       return `It's 8 a.m., ${dailyChoice.dailyChoice.dailyDecision.runningTime}.`;
@@ -74,63 +76,45 @@ showOnPage(
   "<b>--------------------Homework Assignments Weekly Routine---------------------<b>"
 );
 
-// Have a Monday to Friday for doing Homework Assignments.
-// Code showing how my homework assignments routine goes Monday through Friday.
-
-const makeGuess1 = {
-  min1: 7,
-  max1: 10,
-  randomizeNum1: function () {
-    return (makeGuess1.randomizeNum1 =
-      Math.floor(Math.random() * (makeGuess1.max1 - makeGuess1.min1 + 1)) +
-      makeGuess1.min1);
-  },
+const randomizeNum = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
-const makeGuess2 = {
-  min2: 7,
-  max2: 10,
-  randomizeNum2: function () {
-    return (makeGuess2.randomizeNum2 =
-      Math.floor(Math.random() * (makeGuess2.max2 - makeGuess2.min2 + 1)) +
-      makeGuess2.min2);
-  },
-};
-
-const makeGuess3 = {
-  min3: 7,
-  max3: 10,
-  randomizeNum3: function () {
-    return (makeGuess3.randomizeNum3 =
-      Math.floor(Math.random() * (makeGuess3.max3 - makeGuess3.min3 + 1)) +
-      makeGuess3.min3);
-  },
-};
-
-console.log(makeGuess1.randomizeNum1());
-console.log(makeGuess2.randomizeNum2());
-console.log(makeGuess3.randomizeNum3());
 
 showOnPage(`<br><b>Homework Assignments Monday through Friday</b>`);
-dailyChoice.homeworkCheck(35, "Monday Homework Assignments", makeGuess1.randomizeNum1, 7);
-dailyChoice.homeworkCheck(28, "Tuesday Homework Assignments", makeGuess2.randomizeNum2, 7);
-dailyChoice.homeworkCheck(20, "Wednesday Homework Assignments", makeGuess3.randomizeNum3, 7);
-dailyChoice.homeworkCheck(13, "Thursday Homework Assignments", makeGuess1.randomizeNum1, 7);
-dailyChoice.homeworkCheck(6, "Friday Homework Assignments", makeGuess2.randomizeNum2, 6);
+dailyChoice.homeworkCheck(
+  35,
+  "Monday Homework Assignments",
+  randomizeNum(5, 10),
+  7
+);
+dailyChoice.homeworkCheck(
+  28,
+  "Tuesday Homework Assignments",
+  randomizeNum(5, 10),
+  7
+);
+dailyChoice.homeworkCheck(
+  21,
+  "Wednesday Homework Assignments",
+  randomizeNum(5, 10),
+  7
+);
+dailyChoice.homeworkCheck(
+  14,
+  "Thursday Homework Assignments",
+  randomizeNum(5, 10),
+  7
+);
+dailyChoice.homeworkCheck(
+  7,
+  "Friday Homework Assignments",
+  randomizeNum(5, 10),
+  7
+);
 showOnPage(
   `<b>${
     dailyChoice.assignmentQuantity - dailyChoice.partOfAssignments
   } homework assignments over the weekend Yay!! Will have a bit of free time to play video games!!</b>`
 );
-
-// const min1 = 7;
-// const max1 = 10;
-// const randomNum1 = Math.floor(Math.random() * (max1 - min1 + 1)) + min1;
-
-// const min2 = 7;
-// const max2 = 10;
-// const randomNum2 = Math.floor(Math.random() * (max2 - min2 + 1)) + min2;
-
-// const min3 = 7;
-// const max3 = 10;
-// const randomNum3 = Math.floor(Math.random() * (max3 - min3 + 1)) + min3;
