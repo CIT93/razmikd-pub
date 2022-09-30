3f678ef09ec4be3e75293c239ddbd4ff790627eb
Razmik Dabaghyan <rdabaghyan@yahoo.com>
2022-09-29 22:57:13 -0700

    w8 learnTogether - fixing connection issue

--- w8/app.js
+++ w8/app.js
@@ -1,196 +1,83 @@
-let form = document.getElementById("form");
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  let firstName = document.getElementById("first-name").value;
-  let lastName = document.getElementById("last-name").value;
-  let weekDay = document.getElementById("week-days").value;
-  const p1 = document.createElement("span");
-  p1.textContent = `Hi ${firstName} ${lastName}! Check your ${weekDay} homework assignments below.`;
-  document.querySelector("#summary").prepend(p1);
-  const heading = document.createElement("h3");
-  heading.textContent = `Hi ${firstName} here is your Todo List!`;
-  printWeekDayInfo(weekDay)
-  document.querySelector("header").appendChild(heading);
-  document.getElementById("first-name").value = "";
-  document.getElementById("last-name").value = "";
-  document.getElementById("week-days").value = "Monday";
-});
-
-const randomizeNum = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-const guess = function (num) {
-  if (num <= 7) {
-    return `There are no unexpected assignments, which will prevent me from continuing with my previously planned weekly routine.`;
-  } else if (num > 7) {
-    return `Unexpected assignment for today. I need to work a little more than previously planned.`;
-  }
+const userInfo = {
+  firstName: "",
+  lastName: "",
 };
 
 const homeworkCheck = [
   {
-    day: "Monday",
-    totalAssignments: 35,
-    dueAssignments: randomizeNum(5, 10),
-    initiallyPlannedAssignments: 7,
-    unplannedDueAssignments: undefined,
-  },
-  {
-    day: "Tuesday",
-    totalAssignments: 28,
-    dueAssignments: randomizeNum(5, 10),
-    initiallyPlannedAssignments: 7,
-    unplannedDueAssignments: undefined,
-  },
-  {
-    day: "Wednesday",
-    totalAssignments: 21,
-    dueAssignments: randomizeNum(5, 10),
-    initiallyPlannedAssignments: 7,
-    unplannedDueAssignments: undefined,
-  },
-  {
-    day: "Thursday",
-    totalAssignments: 14,
-    dueAssignments: randomizeNum(5, 10),
-    initiallyPlannedAssignments: 7,
-    unplannedDueAssignments: undefined,
-  },
-  {
-    day: "Friday",
-    totalAssignments: 7,
-    dueAssignments: randomizeNum(5, 10),
-    initiallyPlannedAssignments: 7,
-    unplannedDueAssignments: undefined,
-  },
-  {
-    day: "Saturday",
-    totalAssignments: 0,
-    dueAssignments: 0,
-    initiallyPlannedAssignments: 0,
-    unplannedDueAssignments: undefined,
-  },
-  {
-    day: "Sunday",
-    totalAssignments: 0,
-    dueAssignments: 0,
-    initiallyPlannedAssignments: 0,
-    unplannedDueAssignments: undefined,
+    day: "",
+    totalAssignments: "",
+    dueAssignments: "",
+    initiallyPlannedAssignments: "",
+    unplannedDueAssignments: "",
   },
 ];
 
-
-const printWeekDayInfo = function (weekDay) {
-  const homeworkForDay = document.createElement("section");
-  const homeworkInfo = homeworkCheck.find(function (a) {
-    return a.day === weekDay
-  })
-homeworkForDay.innerText = `${
-  homeworkInfo.day
-} \n ___________________ \n\n Total Assignments: ${
-  homeworkInfo.totalAssignments
-} \n\n Due Assignments: ${
-  homeworkInfo.dueAssignments
-}  \n\n Initially Planned Assignments: ${
-  homeworkInfo.initiallyPlannedAssignments
-}  \n\n Unplanned Due Assignments: ${guess(
-  homeworkInfo.dueAssignments
-)} \n\n`;
-document.querySelector("#output").appendChild(homeworkForDay);
-
-}
-
-
-const dailyDecisions = [
-  {
-    text: "Running time",
-    completed: true,
-  },
-  {
-    text: "Email check",
-    completed: true,
-  },
-  {
-    text: "Lunch break",
-    completed: true,
-  },
-  {
-    text: "Walk my dog",
-    completed: true,
-  },
-  {
-    text: "Spend time with family and play ps5",
-    completed: true,
-  },
-  {
-    text: "Zoom meeting",
-    completed: false,
-  },
-  {
-    text: "Do Homework",
-    completed: false,
-  },
-];
-
-const filters = {
-  searchText: "",
-  hideCompleted: false,
+const loginUser = function () {
+  userInfo.firstName = document.getElementById("first-name").value;
+  userInfo.lastName = document.getElementById("last-name").value;
+  document.getElementById("login-page").remove();
 };
 
-const renderTodos = function (dailyDecisions, filters) {
-  const filteredTodos = dailyDecisions.filter(function (todo) {
-    const searchTextMatch = todo.text
-      .toLowerCase()
-      .includes(filters.searchText.toLowerCase());
-    const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
-    return searchTextMatch && hideCompletedMatch;
+const addNewHomework = function () {
+  homeworkCheck.push({
+    day: document.getElementById("week-days").value,
+    totalAssignments: document.getElementById("total-assignments").value,
+    dueAssignments: document.getElementById("due-assignments").value,
+    initiallyPlannedAssignments: document.getElementById(
+      "initially-planned-assignments"
+    ).value,
+    unplannedDueAssignments: document.getElementById(
+      "unplanned-due-assignments"
+    ).value,
   });
-
-  const incompleteDailyDecisions = filteredTodos.filter(function (
-    dailyDecision
-  ) {
-    return !dailyDecision.completed;
-  });
-
-  document.querySelector("#todos").innerHTML = "";
-
-  const total = document.createElement("h4");
-  total.textContent = `You have ${incompleteDailyDecisions.length} daily decisions left for today!`;
-  document.querySelector("#todos").appendChild(total);
-
-  filteredTodos.forEach(function (dailyDecision) {
-    const p = document.createElement("p");
-    p.textContent = dailyDecision.text;
-    document.querySelector("#todos").appendChild(p);
-  });
+  document.getElementById("week-days").value = "Monday";
+  document.getElementById("total-assignments").value = "";
+  document.getElementById("due-assignments").value = "";
+  document.getElementById("initially-planned-assignments").value = "";
+  document.getElementById("unplanned-due-assignments").value = "";
 };
 
-renderTodos(dailyDecisions, filters);
+const showHomeworks = function (day) {
+  const homeworksOutput = document.getElementById("homeworks-output");
+  homeworksOutput.innerHTML = "";
+  const homeworks = homeworkCheck.filter(function (homework) {
+    return homework.day === day;
+  });
+  for (let i = 0; i < homeworks.length; i++) {
+    const homework = homeworks[i];
+    const paragraph = document.createElement("p");
+    paragraph.innerText = `${homework.day} \n ___________________ \n\n Total Assignments: ${homework.totalAssignments} \n\n Due Assignments: ${homework.dueAssignments}  \n\n Initially Planned Assignments: ${homework.initiallyPlannedAssignments}  \n\n Unplanned Due Assignments: ${homework.dueAssignments} \n\n`;
+    homeworksOutput.append(paragraph);
+  }
+  document.getElementById("homework-list-week-days").value = "Monday";
+};
 
-document.querySelector("#search-text").addEventListener("input", function (e) {
-  filters.searchText = e.target.value;
-  renderTodos(dailyDecisions, filters);
+const showMainPage = function () {
+  document.getElementById("main-page").style.visibility = "visible";
+  const fullName = document.createElement("h3");
+  fullName.innerText = `Hi ${userInfo.firstName} ${userInfo.lastName}!!! \n Create a Homework plan for your day using the tool below.`;
+  document.querySelector("#main-page").append(fullName);
+};
+
+const loginForm = document.getElementById("login-form");
+
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  loginUser();
+  showMainPage();
 });
 
-document
-  .querySelector("#add-todo-form")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
-    dailyDecisions.push({
-      text: e.target.elements.newTodo.value,
-      completed: false,
-    });
-    renderTodos(dailyDecisions, filters);
-    e.target.elements.newTodo.value = "";
-  });
+const createHomework = document.getElementById("add-new-homework-form");
 
-document
-  .querySelector("#hide-completed")
-  .addEventListener("change", function (e) {
-    filters.hideCompleted = e.target.checked;
-    renderTodos(dailyDecisions, filters);
-  });
+createHomework.addEventListener("submit", function (e) {
+  e.preventDefault();
+  addNewHomework();
+});
+
+const showHomework = document.getElementById("show-homeworks");
+
+showHomework.addEventListener("click", function (e) {
+  const day = document.querySelector("#homework-list-week-days");
+  showHomeworks(day.value);
+});
